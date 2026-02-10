20471bc9f1af4e63273efcf91ec6026272748e08
Flora <flora.ortiz@ucb.edu.bo>
2026-02-10 06:48:54 -0400

    feat: Route entity

---
+++ Entities/Route.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoFinalTecWeb.Entities
+{
+    public class Route
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } 
+        public string Description { get; set; }
+        public ICollection<Trip> trips { get; set; } = new List<Trip>();
+    }
+}
